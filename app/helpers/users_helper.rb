8a9edec611a2f2ee88f31126bc65cffce9eb6b82
pham.thanh.namb <SUN-ASTERISK\pham.thanh.namb@b123121-pc.sun-asterisk.com>
2022-07-20 09:06:19 +0700

    Edit and Delete User

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,10 @@
 module UsersHelper
   def gravatar_for user, size: Settings.gravatar.default_size
+    if user.avatar.attached?
+      return image_tag(user.display_avatar, alt: user.full_name, class:
+                      "gravatar")
+    end
+
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
     gravatar_url = "#{Settings.gravatar.url}#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.full_name, class: "gravatar")
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 User.create!(
   full_name: "Admin User",
-  email: "test@gmail.com",
+  email: "test123@gmail.com",
   phone: "0326222333",
   password: "123456",
   password_confirmation: "123456",
@@ -8,3 +8,18 @@ User.create!(
   role: :admin
   activation_at: Time.zone.now
 )
+
+99.times do |n|
+  name = Faker::Name.name
+  email = "example-#{n+400}@railstutorial.org"
+  password = "password"
+  User.create!(
+    full_name: name,
+    email: email,
+    password: password,
+    password_confirmation: password,
+    activation: true,
+    role: :normal,
+    activation_at: Time.zone.now
+  )
+end
