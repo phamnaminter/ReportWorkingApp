5ab99450d69a0a608dea8e7c6fbce861756a83f3
pham.thanh.namb <SUN-ASTERISK\pham.thanh.namb@b123121-pc.sun-asterisk.com>
2022-07-25 14:45:46 +0700

    List and show report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,12 @@
 class ReportsController < ApplicationController
   before_action :find_department, :find_manager, :check_access,
                 only: %i(new create)
+  before_action :find_relationship, :paginate_reports, only: :index
+  before_action :find_report, only: :show
+
+  def index; end
+
+  def show; end
 
   def new
     @report = Report.new
@@ -41,5 +47,43 @@ class ReportsController < ApplicationController
 
     flash[:warning] = t "access_denied"
     redirect_to root_path
+  end
+
+  def find_relationship
+    @relationship = Relationship.find_by department_id: params[:department_id],
+                                         user_id: current_user.id
+    return if @relationship.present?
+
+    flash[:warning] = t ".invalid_relationship"
+    redirect_to root_path
+  end
+
+  def find_report
+    @report = Report.find params[:id]
+  end
+
+  def paginate_reports
+    @pagy, @reports = pagy find_all_reports, items: Settings.report.per_page
+  end
+
+  def filter_report
+    filter = params[:filter]
+    return @reports unless filter
+
+    @reports = @reports.by_id(filter[:id])
+                       .by_department(filter[:department])
+                       .by_name(filter[:name])
+                       .by_created_at(filter[:date_created])
+                       .by_report_date(filter[:date_reported])
+                       .sort_created_at
+  end
+
+  def find_all_reports
+    @reports = if @relationship&.manager? || current_user.admin?
+                 Report.for_manager params[:department_id]
+               else
+                 Report.for_employee current_user.id
+               end
+    filter_report
   end
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -3,7 +3,13 @@ class Report < ApplicationRecord
     today_problem tomorow_plan to_user_id).freeze
   belongs_to :from_user, class_name: User.name
   belongs_to :to_user, class_name: User.name
+  belongs_to :department
   has_many :comments, dependent: :destroy
+
+  enum report_status: {unverifyed: 0, confirmed: 1}
+
+  delegate :full_name, to: :from_user
+  delegate :name, to: :department
 
   validates :report_date, presence: true,
             length: {maximum: Settings.digits.length_50}
@@ -19,4 +25,38 @@ class Report < ApplicationRecord
 
   validates :tomorow_plan, presence: true,
             length: {maximum: Settings.digits.length_255}
+
+  scope :sort_created_at, ->{order :created_at}
+
+  scope :by_department, (lambda do |department_name|
+    if department_name.present?
+      Report.joins(:department).where("name LIKE (?)", "%#{department_name}%")
+    end
+  end)
+
+  scope :by_name, (lambda do |name|
+    if name.present?
+      Report.joins(:from_user).where("full_name LIKE (?)", "%#{name}%")
+    end
+  end)
+
+  scope :by_id, (lambda do |id|
+    where(id: id) if id.present?
+  end)
+
+  scope :by_created_at, (lambda do |created_at|
+    where(created_at: created_at) if created_at.present?
+  end)
+
+  scope :by_report_date, (lambda do |report_date|
+    where(report_date: report_date) if report_date.present?
+  end)
+
+  scope :for_manager, (lambda do |department_id|
+    where(department_id: department_id) if department_id
+  end)
+
+  scope :for_employee, (lambda do |user_id|
+    where(from_user_id: user_id) if user_id
+  end)
 end
