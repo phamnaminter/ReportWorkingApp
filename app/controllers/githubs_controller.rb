c1de67636ecb7d540364d56721a4952690bec2d3
Nam's MacBook Pro <phamthanhnam@Namss-MacBook-Pro.local>
2023-02-23 13:46:02 +0700

    OK

--- app/controllers/githubs_controller.rb
+++ app/controllers/githubs_controller.rb
@@ -2,13 +2,11 @@ class GithubsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    debugger
-
     @payload = JSON.parse(params[:payload])
 
     case request.env["HTTP_X_GITHUB_EVENT"]
     when "pull_request"
-      if @payload["action"] == "opened"
+      if @payload["action"] == "opened" || @payload["action"] == "reopened"
         process_pull_request(@payload["pull_request"])
       end
     end
@@ -17,11 +15,13 @@ class GithubsController < ApplicationController
   private
 
   def process_pull_request pull_request
+    @client ||= Octokit::Client.new(access_token: "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")
+
     @client.create_status(pull_request["base"]["repo"]["full_name"], pull_request["head"]["sha"], "pending")
 
-    sleep 2 # do busy work...
+    # sleep 2 # do busy work...
 
-    @client.create_status(pull_request["base"]["repo"]["full_name"], pull_request["head"]["sha"], "success")
-    puts "Pull request processed!"
+    # @client.create_status(pull_request["base"]["repo"]["full_name"], pull_request["head"]["sha"], "success")
+
   end
 end
